6359c20fc8038a7c3e54675644199c4203111676
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-09-06 19:57:59 +0900

    環境構築をする

--- scripts/measure.py
+++ scripts/measure.py
@@ -16,7 +16,6 @@ __status__ = "Production"
 import argparse
 import concurrent.futures
 import math
-import os
 import pathlib
 import subprocess
 import sys
@@ -26,13 +25,10 @@ from typing import *
 logger = getLogger(__name__)
 
 
-def gen(*, seeds: List[int]) -> None:
+def gen(*, seed: int) -> bytes:
     logger.info('running the generator...')
-    with open('seeds.txt', 'w') as fh:
-        for seed in seeds:
-            print(seed, file=fh)
-    command = [str((pathlib.Path.cwd() / 'tools' / 'target' / 'release' / 'gen').resolve()), 'seeds.txt']
-    subprocess.check_call(command)
+    command = [sys.executable, str(pathlib.Path('tester', 'generator.py')), str(seed)]
+    return subprocess.check_output(command)
 
 
 def run(*, command: str, input_path: pathlib.Path, output_path: pathlib.Path, seed: int) -> None:
@@ -45,18 +41,17 @@ def run(*, command: str, input_path: pathlib.Path, output_path: pathlib.Path, se
         logger.exception('failed for seed = %d', seed)
 
 
-def vis(*, input_path: pathlib.Path, output_path: pathlib.Path, vis_path: pathlib.Path, seed: int) -> int:
+def judge(*, input_path: pathlib.Path, output_path: pathlib.Path, seed: int) -> int:
     logger.info('running the visualizer for seed %d...', seed)
     try:
-        command = [str((pathlib.Path.cwd() / 'tools' / 'target' / 'release' / 'vis').resolve()), str(input_path), str(output_path)]
+        command = [sys.executable, str(pathlib.Path('tester', 'judge.py')), str(input_path), str(output_path)]
         score_bytes = subprocess.check_output(command)
     except subprocess.SubprocessError:
         logger.exception('failed for seed = %d', seed)
         return 0
-    os.rename('out.svg', vis_path)
-    if not score_bytes.startswith(b'Score = '):
+    if not score_bytes.startswith(b'score:'):
         raise RuntimeError(score_bytes.decode())
-    return int(score_bytes.split()[2])
+    return int(score_bytes.split(b':')[1])
 
 
 def main() -> 'NoReturn':
@@ -70,37 +65,38 @@ def main() -> 'NoReturn':
 
     basicConfig(level=DEBUG)
 
-    if not pathlib.Path('tools').exists():
-        logger.error('tools/ directory is not found')
+    if not pathlib.Path('tester').exists():
+        logger.error('tester/ directory is not found')
         sys.exit(1)
-    command = ['cargo', 'build', '--manifest-path', str(pathlib.Path('tools', 'Cargo.toml')), '--release']
-    subprocess.check_output(command)
 
     pathlib.Path('in').mkdir(exist_ok=True)
     pathlib.Path('out').mkdir(exist_ok=True)
-    pathlib.Path('vis').mkdir(exist_ok=True)
 
     # gen
     if args.same:
         seeds = [args.seed for i in range(args.count)]
     else:
         seeds = [args.seed + i for i in range(args.count)]
-    gen(seeds=seeds)
+    for seed in seeds:
+        input_path = pathlib.Path('in', 'input_%d.txt' % seed)
+        if not input_path.exists():
+            input_data = gen(seed=seed)
+            with open(input_path, 'wb') as fh:
+                fh.write(input_data)
 
     # run
     with concurrent.futures.ThreadPoolExecutor(max_workers=args.jobs) as executor:
         for i, seed in enumerate(seeds):
-            input_path = pathlib.Path('in', '%04d.txt' % i)
-            output_path = pathlib.Path('out', '%04d.txt' % i)
+            input_path = pathlib.Path('in', 'input_%d.txt' % i)
+            output_path = pathlib.Path('out', 'output_%d.txt' % i)
             executor.submit(run, command=args.command, input_path=input_path, output_path=output_path, seed=seed)
 
     # vis
     scores: List[int] = []
     for i, seed in enumerate(seeds):
-        input_path = pathlib.Path('in', '%04d.txt' % i)
-        output_path = pathlib.Path('out', '%04d.txt' % i)
-        vis_path = pathlib.Path('vis', '%04d.svg' % i)
-        score = vis(input_path=input_path, output_path=output_path, vis_path=vis_path, seed=seed)
+        input_path = pathlib.Path('in', 'input_%d.txt' % i)
+        output_path = pathlib.Path('out', 'output_%d.txt' % i)
+        score = judge(input_path=input_path, output_path=output_path, seed=seed)
         scores.append(score)
         logger.info('seed = {}: score = {}'.format(seed, score))
     average = sum(scores) / len(scores)
